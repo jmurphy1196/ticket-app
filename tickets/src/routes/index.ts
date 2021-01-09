d0a547803e3fc0a03d1c99de3294b472936a5c9c
jason murphy <jmurphy.1196@gmail.com>
2021-01-09 04:44:57 -0800

    cleaned up index

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get(
   "/api/tickets",
   async (req: Request, res: Response, next: NextFunction) => {
-    const tickets = await Ticket.find({});
+    const tickets = await Ticket.find({ orderId: undefined });
     res.status(200).send(tickets);
   }
 );
